f0e10f1354c743be34ec59f9057710a50f38ddad
JoseAntonio5 <jlopespaiva00@gmail.com>
2022-10-22 18:57:26 -0300

    add footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import UserInput from './components/UserInput';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <UserInput />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className='Footer'>José Antônio &copy; 2022</div>
+  )
+}
+
+export default Footer;
